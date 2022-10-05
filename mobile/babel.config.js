62bd47d8c0447a32c38d4ba346c366f5c7edeb42
Jarosław Leśniak <jlesniak@itisit.pl>
2022-10-05 23:38:05 +0200

    Added aliases

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '~assets': './assets',
+          '~test-utils': './test-utils',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '~env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
