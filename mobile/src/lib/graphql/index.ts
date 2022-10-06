7a9f503ecfd6f56468c2b7669af3ce71644c45af
Jarosław Leśniak <jlesniak@itisit.pl>
2022-10-06 20:49:02 +0200

    Fixed env variabels

--- mobile/src/lib/graphql/index.ts
+++ mobile/src/lib/graphql/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 import { API_URL } from '~env';
-console.log(API_URL);
+
 export const client = new ApolloClient({
   uri: API_URL,
   cache: new InMemoryCache(),
